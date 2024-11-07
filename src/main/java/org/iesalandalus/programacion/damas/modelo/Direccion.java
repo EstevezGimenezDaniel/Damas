e3f03dcac67900c6235e2bb661aef4aa2e5b0d27
Daniel Estevez <daniestevez21@gmail.com>
2024-11-07 21:05:14 +0100

    Literales parametrizados con cadena de los mismos

---
+++ src/main/java/org/iesalandalus/programacion/damas/modelo/Direccion.java
@@ -0,0 +1,13 @@
+package org.iesalandalus.programacion.damas.modelo;
+
+public enum Direccion {
+
+    NORESTE("Noreste"),
+    SURESTE("Sureste"),
+    SUROESTE("Suroeste"),
+    NOROESTE("Noroeste");
+
+
+
+
+}
