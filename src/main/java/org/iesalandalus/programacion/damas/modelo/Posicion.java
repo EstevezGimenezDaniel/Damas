726c65a4b7cbc5cf27c4be2dc3ad85e640533316
Daniel Estevez <daniestevez21@gmail.com>
2024-11-07 21:28:11 +0100

    Terminada clase Posicion

---
+++ src/main/java/org/iesalandalus/programacion/damas/modelo/Posicion.java
@@ -0,0 +1,57 @@
+package org.iesalandalus.programacion.damas.modelo;
+
+import java.util.Objects;
+
+public class Posicion {
+
+    private int fila;
+    private char columna;
+
+    public Posicion(int fila, char columna) {
+        setFila(fila);
+        setColumna(columna);
+    }
+
+    public Posicion(Posicion otraPosicion) {
+        this(otraPosicion.fila, otraPosicion.columna);
+    }
+
+    public int getFila() {
+        return fila;
+    }
+
+    public char getColumna() {
+        return columna;
+    }
+    public void setFila(int fila) {
+        if (fila < 1 || fila > 8) {
+            throw new IllegalArgumentException("La fila debe ser entre 1 y 8");
+        }
+        this.fila = fila;
+    }
+
+    public void setColumna(char columna) {
+        if (columna < 'a' || columna > 'h') {
+            throw new IllegalArgumentException("La columna debe ser entre a y h");
+        }
+        this.columna = columna;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Posicion posicion = (Posicion) obj;
+        return fila == posicion.fila && columna == posicion.columna;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(fila, columna);
+    }
+
+    @Override
+    public String toString() {
+        return "fila=" + fila + ", columna=" + columna;
+    }
+}
