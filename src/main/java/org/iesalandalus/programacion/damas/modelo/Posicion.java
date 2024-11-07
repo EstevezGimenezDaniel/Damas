459f3536fc6c5a7872409694e5e017bb233630cc
Daniel Estevez <daniestevez21@gmail.com>
2024-11-07 22:13:02 +0100

    Corrección clase Posicion

--- src/main/java/org/iesalandalus/programacion/damas/modelo/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/damas/modelo/Posicion.java
@@ -16,25 +16,23 @@ public class Posicion {
         this(posicion.fila, posicion.columna);
     }
 
+    public int getFila() {
+        return fila;
+    }
     public void setFila(int fila) {
         if (fila < 1 || fila > 8) {
             throw new IllegalArgumentException("La fila debe ser entre 1 y 8");
         }
         this.fila = fila;
     }
-
+    public char getColumna() {
+        return columna;
+    }
     public void setColumna(char columna) {
         if (columna < 'a' || columna > 'h') {
             throw new IllegalArgumentException("La columna debe ser entre a y h");
         }
         this.columna = columna;
-    }
-    public int getFila() {
-        return fila;
-    }
-
-    public char getColumna() {
-        return columna;
     }
 
     @Override
